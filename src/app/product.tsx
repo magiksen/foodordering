01990b132d1c9826015b4cca06e4e99afff5d316
Magiksen <elemejore@gmail.com>
2024-02-09 15:46:04 -0400

    Link to single product page 1

---
+++ src/app/product.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const ProductDetailsScreen = () => {
+  return (
+    <View>
+      <Text>ProductDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,13 +1,27 @@
-import { StyleSheet, Text, View, Image  } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '@/src/constants/Colors';
+import { Product } from '../types';
+import { Link } from 'expo-router';
 
-const ProductListItem = ({ product }) => {
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+  product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode='contain'
+      />
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
+
+      <Link href={"/product"}>Go to details</Link>
     </View>
   );
 };
@@ -19,6 +33,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
   },
   image: {
     width: '100%',
